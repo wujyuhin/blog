30c204cdd3610370e43df1e6305620f019aa46f7
wujyuhin <wujyuhin@163.com>
2024-10-28 10:15:31 +0800

    Quartz sync: Oct 28, 2024, 10:15 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🪴 胡誉骞的博客",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
